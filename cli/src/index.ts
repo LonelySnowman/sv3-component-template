bbe2ebdf68e5c3edca070488bbd41365854a2c1e
LonelySnowman <2438490168@qq.com>
2023-04-05 19:54:45 +0800

    feat: 新增cli

---
+++ cli/src/index.ts
@@ -0,0 +1,4 @@
+
+export const mainEntry = () => {
+  console.log('hello cli mainEntry')
+}
--- packages/foo/index.ts
+++
@@ -1,8 +0,0 @@
-import { App } from 'vue'
-import Foo from './src/index'
-
-Foo.install = (app: App) => {
-  app.component(Foo.name, Foo)
-}
-
-export default Foo
